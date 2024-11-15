18db7c73985b2bb66d98ced84bbda527bc26aa7b
shrey-kumbhare <116801075+shrey-kumbhare@users.noreply.github.com>
2024-11-16 00:15:13 +0530

    Home

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import CustomerForm from "../components/Ingestion/CustomerForm";
+import OrderForm from "../components/Ingestion/OrderForm";
+import AudienceForm from "../components/Ingestion/AudienceForm";
+import CampaignList from "../components/CampaignListing/CampaignList";
+import HomePage from "../pages/HomePage";
+import "./Home.css";
+
+const Home = ({ onLogout }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await onLogout();
+    navigate("/login", { replace: true });
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/home/customers">Create Customer</Link>
+          </li>
+          <li>
+            <Link to="/home/orders">Create Order</Link>
+          </li>
+          <li>
+            <Link to="/home/audience">Create Audience</Link>
+          </li>
+          <li>
+            <Link to="/home/campaigns">Campaign List</Link>
+          </li>
+          <li className="logout-button">
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="content">
+        <Routes>
+          <Route path="/customers" element={<CustomerForm />} />
+          <Route path="/orders" element={<OrderForm />} />
+          <Route path="/audience" element={<AudienceForm />} />
+          <Route path="/campaigns" element={<CampaignList />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// components/HomePage.js
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1 className="home-page-title">Welcome to the CRM Application</h1>
+    </div>
+  );
+};
+
+export default HomePage;
